7a87c31812529153cd379dad208f97f7bf785b23
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-06-16 17:21:36 +0300

    Working on contacts

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,12 @@
 // import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 // import { flsModules } from "./modules.js";
+//
+document.addEventListener("DOMContentLoaded", function (e) {
+  const fileInputs = document.querySelectorAll('input[type="file"]');
+  if (fileInputs.length) {
+    fileInputs.foeEach(input, (e) => {
+      console.log(input);
+    });
+  }
+});
