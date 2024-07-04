651c0cc036dc18fb11e30d50d3c90963f88ae84a
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-07-04 13:42:55 +0300

    make script for opening blocks, include dynamic adapt

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,13 +1,13 @@
 // Подключение функционала "Чертогов Фрилансера"
-// import { isMobile } from "./functions.js";
+import { _slideUp, _slideDown, _slideToggle } from "./functions.js";
 // Подключение списка активных модулей
 // import { flsModules } from "./modules.js";
 //
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e) {
   const fileInputs = document.querySelectorAll('input[type="file"]');
   if (fileInputs.length) {
     fileInputs.forEach((input) => {
-      input.addEventListener("change", function (e) {
+      input.addEventListener("change", function(e) {
         const fileName = e.target.files[0].name;
         e.target
           .closest("label")
@@ -15,4 +15,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
       });
     });
   }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const functionShowMoreBlocks = document.querySelectorAll(".functions");
+
+  functionShowMoreBlocks.forEach((block) => {
+    const showMoreButton = block.querySelector("[data-showmore-button]");
+    const showMoreContent = block.querySelector("[data-showmore-content]");
+
+    showMoreButton.addEventListener("click", () => {
+      showMoreButton.classList.toggle("_active");
+      _slideToggle(showMoreContent);
+    });
+  });
 });
